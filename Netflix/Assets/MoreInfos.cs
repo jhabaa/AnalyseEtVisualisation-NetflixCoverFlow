8ca6f5b3624d431db1065c0fbe5d1e77d6d559f7
Jean Hubert ABA'A <78347379+jhabaa@users.noreply.github.com>
2021-12-09 08:26:25 +0100

    terminé

---
+++ Netflix/Assets/MoreInfos.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreInfos : MonoBehaviour
+{
+    public TextAsset csv;
+    private string[][] grid;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = CsvParser2.Parse(csv.text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z >= -20.6 && this.transform.position.x <= 0.4f && this.transform.position.x >= 0.2f)
+        {
+            Vector3 largeSize = new Vector3(this.transform.position.x,
+                                                    this.transform.position.y,
+                                                    this.transform.position.z - 0.1f);
+            this.transform.position = Vector3.MoveTowards(this.transform.position, largeSize, 1f * Time.deltaTime);
+        }
+        if(Input.GetMouseButton(0) == true)
+        {
+            
+        }
+    }
+    private void OnMouseEnter()
+    {
+        //this.transform.localScale = this.transform.localScale * 1.002f;
+      /*  this.transform.localPosition = new Vector3(this.transform.localPosition.x,
+                                                    this.transform.localPosition.y,
+                                                    this.transform.localPosition.z - 0.6f);*/
+        
+    }
+    private void OnMouseExit()
+    {
+        
+        //this.transform.localScale = this.transform.localScale / 1.002f;
+    }
+    private void OnMouseDown()
+    {
+        /*
+        Vector3 direction = new Vector3(-50, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+        Quaternion targetRotation = Quaternion.Euler(direction);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 1);*/
+    }
+}
---
+++ Netflix/Assets/ntflix.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ntflix : MonoBehaviour
+{
+    public TextAsset csv, imagesCSV;
+    private string[][] grid, imagesGrid ;
+    public Button next, left, right;
+    public GameObject cover;
+    public List<GameObject> coverflow;
+    public Material metal;
+    public Text text;
+    private int a=1 ,b = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //metal = Resources.Load("metal", typeof(Material)) as Material;
+        Button rightButton = right.GetComponent<Button>();
+        rightButton.onClick.AddListener(ShowNext);
+        Button leftButton = left.GetComponent<Button>();
+        leftButton.onClick.AddListener(ShowPrev);
+        Button nextButton = next.GetComponent<Button>();
+        nextButton.onClick.AddListener(createFlow);
+        grid = CsvParser2.Parse(csv.text);
+        imagesGrid = CsvParser2.Parse(imagesCSV.text);
+    }
+
+    private void ShowPrev()
+    {
+        foreach (GameObject gameObject in coverflow)
+        {
+            Vector3 nextPos = new Vector3(gameObject.transform.position.x + 25, 0, -20);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextPos, 2000f * Time.deltaTime);
+        }
+    }
+
+    private void ShowNext()
+    {
+        foreach(GameObject gameObject in coverflow)
+        {
+            Vector3 nextPos = new Vector3(gameObject.transform.position.x - 25, 0, -20);
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextPos, 2000f * Time.deltaTime);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("left") == true)
+        {
+            foreach (GameObject gameObject in coverflow)
+            {
+                Vector3 nextPos = new Vector3(gameObject.transform.position.x + 25, 0, -20);
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextPos, 2000f * Time.deltaTime);
+            }
+        }
+        if (Input.GetKeyDown("right") == true)
+        {
+            foreach (GameObject gameObject in coverflow)
+            {
+                Vector3 nextPos = new Vector3(gameObject.transform.position.x - 25, 0, -20);
+                gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, nextPos, 4000f * Time.deltaTime);
+            }
+        }
+        /*
+        print(grid.Length);
+        print(grid[0].Length);
+        print(grid[3][4]);*/
+    }
+    private void createFlow()
+    {
+        Debug.Log("Test");
+        for (a = b; a < b+50; a++)
+        {
+            GameObject g = GameObject.Instantiate(cover);
+            //g.transform.position = new Vector3(a+35, 0, -20);
+            g.GetComponent<MeshRenderer>().material = metal;
+            g.AddComponent<MoreInfos>();
+            g.GetComponent<MoreInfos>().csv = csv;
+            //g.AddComponent<Rigidbody>();
+            //g.GetComponent<Rigidbody>().useGravity = false;
+            //g.GetComponent<Rigidbody>().isKinematic = true;
+            g.name = "New";
+            coverflow.Add(g);
+            //g.GetComponent<stickyText>().text = text;
+            
+            var gTitle = g.transform.GetChild(0).GetChild(3).gameObject;
+            var gCast = g.transform.GetChild(0).GetChild(1).gameObject;
+            var gDescription = g.transform.GetChild(0).GetChild(2).gameObject;
+            var gImage = g.transform.GetChild(0).GetChild(0).gameObject;
+            gTitle.GetComponent<TextMeshProUGUI>().text = grid[a][2];
+            gCast.GetComponent<TextMeshProUGUI>().text = grid[a][10];
+            gDescription.GetComponent<TextMeshProUGUI>().text = grid[a][9];
+            gImage.GetComponent<LoadOnlineImageToCanvas>().TextureURL = grid[a][4].ToString();
+        }
+        b = b + 50;
+        foreach (GameObject gameObject in coverflow)
+        {
+            if(coverflow.IndexOf(gameObject) == 0)
+            {
+
+            }
+            else if(coverflow.IndexOf(gameObject) != 0)
+            {
+                GameObject p = coverflow[coverflow.IndexOf(gameObject) - 1];
+                gameObject.transform.position = new Vector3(p.transform.position.x + 25, 0, -20);
+            }
+            
+        }
+        
+    }
+
+}
